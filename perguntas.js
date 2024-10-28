0380ed34c1b1c66611d41ee4151f2ab0bf2baebd
cachinhos10 <161498203+cachinhos10@users.noreply.github.com>
2024-10-28 11:51:05 -0300

    f

---
+++ perguntas.js
@@ -0,0 +1,26 @@
+criaCartao(
+    "Programação",
+    "O que é Python?",
+    " O Python é uma linguagm de programação"
+)
+
+criaCartao(
+    "Geografia",
+    "Qual a capital da frança?",
+    "A capital a França é Paris"
+
+)
+
+criaCartao(
+    " Programação",
+    "O que é uma Função?",
+    "Uma Função é um bloco de código que executa alguma tarfa"
+
+)
+
+criaCartao(
+    "Lingua inglea",
+    " Como se diz oi em Inglẽs?",
+    "Oi em igles é HI (RAI)"
+    
+)
